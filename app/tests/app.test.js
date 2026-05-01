e284d5f5af41bd4cbafe640f239421cca4b4a0af
Jouini-Nour <nour.jouini.558@gmail.com>
2026-05-01 23:23:12 +0100

    Initial commit

---
+++ app/tests/app.test.js
@@ -0,0 +1,15 @@
+const request = require('supertest');
+
+const app = require('../server');
+
+describe('GET /', () => {
+
+    it('should return 200', async () => {
+
+        const res = await request(app).get('/');
+
+        expect(res.statusCode).toEqual(200);
+
+    });
+
+});
